1aab2d06858557ebdcce2b74d14be5e1f5877ee3
Jimmy <odylic.lin@gmail.com>
2021-11-16 14:49:08 -0800

    welcome person, added default linting to ts files

--- app.ts
+++ app.ts
@@ -1,6 +1,17 @@
-console.log('hello world')
+console.log("hello world");
 
+function welcomePerson(person: Person) {
+  console.log(`Hey ${person.firstName} ${person.lastName}`);
+}
 
-function welcomePerson(person) {
-  console.log(`Hey ${person.firstName} ${person.lastName}`)
+const james = {
+  firstName: "James",
+  lastName: "Quick",
+};
+
+welcomePerson(james);
+
+interface Person {
+  firstName: string;
+  lastName: string;
 }
